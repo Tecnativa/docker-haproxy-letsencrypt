93b501aa2c89f1a9ea13f73ea12d58fb419347b1
Jairo Llopis <yajo.sk8@gmail.com>
2017-02-01 08:13:59 +0000

    [FIX] Do not die when HAProxy is not yet started.
    This script is called once before launching HAProxy too, and the
    killall part would die there without this patch.

--- bin/reload.sh
+++ bin/reload.sh
@@ -4,4 +4,4 @@ set -x
 # SEE http://cbonte.github.io/haproxy-dconv/1.7/configuration.html#4.2-load-server-state-from-file
 echo show servers state | socat /var/lib/haproxy/stats - > /var/lib/haproxy/server-state
 # SEE https://github.com/docker-library/docs/tree/master/haproxy#reloading-config
-killall -s HUP haproxy-systemd-wrapper
+killall -s HUP haproxy-systemd-wrapper || true
